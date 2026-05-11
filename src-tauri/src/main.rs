49c40550a9cb76180bb23ebf55fd1b1a27d6b2b7
endri Susanto <endrisusantosekarang@gmail.com>
2026-05-11 10:45:35 +0700

    Embed WebKit/Wayland crash fix for Linux packages

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    #[cfg(target_os = "linux")]
+    {
+        // Fix for WebKitGTK/Wayland crashes on some Linux distributions
+        std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+        std::env::set_var("GDK_BACKEND", "x11");
+    }
     atm_runner_lib::run()
 }
